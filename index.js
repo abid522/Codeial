803ea2810d5f280a2d011d7a8f42339355b73d60
Aabid <abid1600251@gmail.com>
2023-05-29 17:15:19 +0530

    Added layout and setup static file access

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = require('./routes/index');
+const expressLayouts = require('express-ejs-layouts');
 const port = 8000;
 
 const app = express();
+
+//Telling express the loaction of static files
+app.use(express.static('./assets'));
+
+//Telling express to use express-ejs-layouts library
+app.use(expressLayouts);
+
+//Extract styles and scripts from subpages to the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //Use express router
 app.use('/', router);
