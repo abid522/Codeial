7fc1a0bdaa04883437737960d5cd5387c6a4f689
Aabid <abid1600251@gmail.com>
2023-07-02 09:23:17 +0530

    Added some async-await version of conyroller actions

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,31 +1,55 @@
 const Post = require('../models/post');
 const User = require('../models/user');
 
-module.exports.home = function (req, res) {
-    // console.log(req.cookies);
-    // res.cookie('user_id', 28);
-    Post.find({})
-        .populate('user')
-        .populate({
-            path: 'comments',
-            populate: {
-                path: 'user'
-            }
-        })
-        .then(posts => {
-            User.find({})
-                .then(users => {
-                    return res.render('home', {
-                        title: 'Codeial | Home',
-                        posts: posts,
-                        users: users
-                    });
-                })
-                .catch(err => {
-                    console.log(err + 'Error fetching users');
-                })
-        })
-        .catch(err => {
-            console.log(err + 'Error fetching posts');
+// module.exports.home = function (req, res) {
+//     // console.log(req.cookies);
+//     // res.cookie('user_id', 28);
+//     Post.find({})
+//         .populate('user')
+//         .populate({
+//             path: 'comments',
+//             populate: {
+//                 path: 'user'
+//             }
+//         })
+//         .then(posts => {
+//             User.find({})
+//                 .then(users => {
+//                     return res.render('home', {
+//                         title: 'Codeial | Home',
+//                         posts: posts,
+//                         users: users
+//                     });
+//                 })
+//                 .catch(err => {
+//                     console.log(err + 'Error fetching users');
+//                 })
+//         })
+//         .catch(err => {
+//             console.log(err + 'Error fetching posts');
+//         });
+// }
+
+module.exports.home = async function (req, res) {
+    try {
+        const posts = await Post.find({})
+            .populate('user')
+            .populate({
+                path: 'comments',
+                populate: {
+                    path: 'user'
+                }
+            });
+
+        const users = await User.find({});
+
+        return res.render('home', {
+            title: 'Codeial | Home',
+            posts: posts,
+            users: users
         });
+    } catch (err) {
+        console.log(err);
+        return res.redirect('back');
+    }
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -18,26 +18,40 @@ module.exports.createPost = function (req, res) {
         });
 }
 
-module.exports.deletePost = function (req, res) {
-    Post.findById(req.params.id)
-        .then(post => {
-            //.id means converting the object id into string
-            if (post.user == req.user.id) {
-                Post.findByIdAndDelete(req.params.id)
-                    .then(data => {
-                        //post deleted and now its turn to delete comments
-                        return Comment.deleteMany({ post: req.params.id });
-                    })
-                    .then(comment => {
-                        return res.redirect('back');
-                    })
-                    .catch(err => {
-                        return console.log(err + 'Error deleting the post');
-                    });
-            }
-        })
-        .catch(err => {
-            console.log(err + 'Error finding the post');
+// module.exports.deletePost = function (req, res) {
+//     Post.findById(req.params.id)
+//         .then(post => {
+//             //.id means converting the object id into string
+//             if (post.user == req.user.id) {
+//                 Post.findByIdAndDelete(req.params.id)
+//                     .then(data => {
+//                         //post deleted and now its turn to delete comments
+//                         return Comment.deleteMany({ post: req.params.id });
+//                     })
+//                     .then(comment => {
+//                         return res.redirect('back');
+//                     })
+//                     .catch(err => {
+//                         return console.log(err + 'Error deleting the post');
+//                     });
+//             }
+//         })
+//         .catch(err => {
+//             console.log(err + 'Error finding the post');
+//             return res.redirect('back');
+//         });
+// }
+
+module.exports.deletePost = async function (req, res) {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (post.user == req.user.id) {
+            await Post.findByIdAndDelete(req.params.id);
+            await Comment.deleteMany({ post: req.params.id });
             return res.redirect('back');
-        });
+        }
+    } catch (err) {
+        console.log(err + 'Error Deleting the post');
+        return res.redirect('back');
+    }
 }
